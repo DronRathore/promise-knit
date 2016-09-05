a7a586dfaeec24eb1ba1366c66ddef51a8c9d362
Dron Rathore <dron.rathore@gmail.com>
2016-09-05 19:36:52 +0530

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 'use strict'
+/*
+  @desc: This function triggers the callback with value passed to it
+  @params:
+    param: An atomic value
+    output: A buffer end-state
+    trigger:  consumer function
+*/
 function feedPromise(param, output, trigger){
   if (typeof trigger != "function"){
     trigger = output
@@ -12,6 +19,12 @@ function feedPromise(param, output, trigger){
     })
   })
 }
+/*
+  @desc: Initialise the promise config
+  @params:
+    params: Array
+    triggers: Function or [Function...]
+*/
 var setupPromise = (params, triggers)=>{
   if (params instanceof Array){
     if (triggers instanceof Array){
@@ -25,6 +38,19 @@ var setupPromise = (params, triggers)=>{
     throw new Error("params passed must be an array")
   }
 }
+/*
+  @desc: Main Promise.knit function
+  @param:
+    params: []
+      > An array list of values to be consumed
+    consumers: [Function..] or Function
+      > An array of Promise Consumer(s)
+    outputStub: Array or Object
+      > Initial Output Stub value to be used by consumers
+    chain: Boolean
+      > Use the output object filled by the first consumer as input
+        for the next consumer
+*/
 Promise.knit = 
   Promise.knit || ((params, consumers, outputStub, chain)=>{
     return new Promise((resolve, reject)=>{
@@ -33,15 +59,21 @@ Promise.knit =
       var nextPromise = (_params, _consumers, _output)=>{
         var value = _params.shift()
         var trigger = _consumers[0]
+        // are we done processing for first consumer?
         if (value == undefined){
           _consumers.shift()
+          // are there any more consumers left
           if (_consumers.length == 0){
+            // no? then resolve the last output state
             return resolve(_output)
           } else {
+            // are we chaining outputs?
             if (!chain){
+              // use single state output and inital params as input
               _params = Array().concat([], config.params)
               return nextPromise(_params, _consumers, _output)
             } else {
+              // use output from last state as input for the next
               _params = output
               return nextPromise(_params, _consumers, [])
             }
@@ -54,6 +86,7 @@ Promise.knit =
           return reject(err)
         })
       }
+      // initialise the whole process via copy of arguments
       return nextPromise(Array().concat([], config.params), Array().concat([], config.triggers), output)
     })
   })
